bc0ea9358b886fca2e2c79c6536ac9d65e22db81
vladyslavJ <vladzhukov321@gmail.com>
2025-05-19 22:51:26 +0300

    added retries

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,5 +1,6 @@
 import nodemailer, { Transporter } from 'nodemailer';
 import type SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { logger } from '../utils/logger';
 import ENV from '../config/env';
 
 const smtpOptions: SMTPTransport.Options = {
@@ -11,14 +12,23 @@ const smtpOptions: SMTPTransport.Options = {
 
 const transporter: Transporter = nodemailer.createTransport(smtpOptions);
 
-transporter
-	.verify()
-	.then(() => {
-		console.log('[MAIL] SMTP connection is OK');
-	})
-	.catch((err) => {
-		console.error('[MAIL] SMTP connection failed', err);
-	});
+async function verifyWithRetry(attempts = 5, delayMs = 5000) {
+	for (let i = 1; i <= attempts; i++) {
+		try {
+			await transporter.verify();
+			logger.info(`[MAIL] SMTP connection is OK (try ${i})`);
+			return;
+		} catch (err) {
+			logger.error(`[MAIL] SMTP connection failed (try ${i})`, err);
+			if (i === attempts) throw err;
+			await new Promise((res) => setTimeout(res, delayMs));
+		}
+	}
+}
+
+verifyWithRetry().catch(() => {
+	logger.error('[MAIL] SMTP not available, emails will not be sent.');
+});
 
 export interface SendOptions {
 	to: string;
